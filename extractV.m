18a17020033eba2a372175abee23de24f1169f5b
tidio634 <tidio634@gmail.com>
2015-09-01 16:03:27 -0400

    added largest decrease method, halfmax method

--- extractV.m
+++ extractV.m
@@ -3,23 +3,52 @@
 % calculates the velocity of the expansion from simulation curves
 
 n = 10;
-
 t = tpoints(end-n+1:end);
 p = sump(:,end-n+1:end);
 
-% half max method
+% % largest decrease method
+% pos = []; val = [];
+% for i = 1:length(t)
+%     pnow = p(:,i);
+%     [M I] = max(-diff(pnow'));
+%     pos = [pos x(I)];
+%     val = [val pnow(I)];
+% end
 
+% % half max method
+% pos = []; val = [];
+% for i = 1:length(t)
+%     pnow = p(:,i);
+%     hm = 0.5*max(pnow);
+%     [M I] = min(abs(pnow-hm));
+%     pos = [pos x(I)];
+%     val = [val pnow(I)];
+% end
+
+% loglog method
+pos = []; val = [];
 for i = 1:length(t)
-
-
+%     pnow = p(:,i);
+    pnow = p(:,end);
+    
+%     hm = 0.5*max(pnow);
+%     [M I] = min(abs(pnow-hm));
+    hm = 0.5*max(pnow);
+    [M I] = min(abs(pnow-hm));
+  
+    pos = [pos x(I)];
+    val = [val pnow(I)];
+     
+    figure; hold on;
+    plot(x,pnow);
+    plot(x(I),pnow(I),'r*');
 end
-figure
-plot(t,halfmax)
 
-
-% figure(1);
-% plot(x, p)
-
+figure;
+plot(t,pos, 'k*')
+figure; hold on;
+plot(x, p)
+plot(pos, val, 'r*' )
 
 % end
 
