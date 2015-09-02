30bbc7ec8f598abf40cd85c1d8f9ab8725ccce3a
tidio634 <tidio634@gmail.com>
2015-09-01 22:48:07 -0400

    extractV done
    somehow the V does not mach v_theoretical

--- extractV.m
+++ extractV.m
@@ -1,4 +1,4 @@
-% function [ velocity ] = extractV(x, tpoints, sump)
+function [ velocity ] = extractV(x, tpoints, sump)
 %EXTRACTV
 % calculates the velocity of the expansion from simulation curves
 
@@ -6,14 +6,14 @@ n = 10;
 t = tpoints(end-n+1:end);
 p = sump(:,end-n+1:end);
 
-% % largest decrease method
-% pos = []; val = [];
-% for i = 1:length(t)
-%     pnow = p(:,i);
-%     [M I] = max(-diff(pnow'));
-%     pos = [pos x(I)];
-%     val = [val pnow(I)];
-% end
+% largest decrease method
+pos = []; val = [];
+for i = 1:length(t)
+    pnow = p(:,i);
+    [M I] = max(-diff(pnow'));
+    pos = [pos x(I)];
+    val = [val pnow(I)];
+end
 
 % % half max method
 % pos = []; val = [];
@@ -25,30 +25,36 @@ p = sump(:,end-n+1:end);
 %     val = [val pnow(I)];
 % end
 
-% loglog method
-pos = []; val = [];
-for i = 1:length(t)
-%     pnow = p(:,i);
-    pnow = p(:,end);
-    
+% % loglog method
+% pos = []; val = [];
+% for i = 1:length(t)
+% %     pnow = p(:,i);
+%     pnow = p(:,end);
+%     
+% %     hm = 0.5*max(pnow);
+% %     [M I] = min(abs(pnow-hm));
 %     hm = 0.5*max(pnow);
 %     [M I] = min(abs(pnow-hm));
-    hm = 0.5*max(pnow);
-    [M I] = min(abs(pnow-hm));
-  
-    pos = [pos x(I)];
-    val = [val pnow(I)];
-     
-    figure; hold on;
-    plot(x,pnow);
-    plot(x(I),pnow(I),'r*');
-end
+%   
+%     pos = [pos x(I)];
+%     val = [val pnow(I)];
+%      
+%     figure; hold on;
+%     plot(x,pnow);
+%     plot(x(I),pnow(I),'r*');
+% end
 
-figure;
-plot(t,pos, 'k*')
 figure; hold on;
 plot(x, p)
 plot(pos, val, 'r*' )
 
-% end
+PS = polyfit(t,pos,1);
+figure; hold on;
+plot(t,pos, 'k*')
+plot(t,t*PS(1)+PS(2),'r')
+
+velocity = PS(1);
+
+
+end
 
--- sweeper.m
+++ sweeper.m
@@ -1,12 +1,13 @@
+clear all; close all;
+
 v1   = 20;  % polymerization
 v2   = 16;  % depolymerization
 fcat = 3; % catastrophe
 fres = 1; % rescue
-r = 1; % nucleation rate
+r    = 0.6; % nucleation rate
+dim  = 1;  % dimension of system
 
-dim = 1;  % dimension of system
-
-% [x, tpoints, sump] = solve_advection(v1,v2,fcat,fres,r,dim);
+[x, tpoints, sump] = solve_advection(v1,v2,fcat,fres,r,dim);
 
 extractV(x,tpoints,sump)
 
