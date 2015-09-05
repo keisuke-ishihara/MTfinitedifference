2c3280c8ef1ba9ef694604fc696f19502eb0fbfb
tidio634 <tidio634@gmail.com>
2015-09-05 16:10:30 -0400

    made function 'theoretical'

--- sweeper.m
+++ sweeper.m
@@ -1,17 +1,19 @@
 clear all; close all;
 
 v1   = 20;  % polymerization
-v2   = 18;  % depolymerization
+v2   = 16;  % depolymerization
 fcat = 3; % catastrophe
 fres = 1; % rescue
-rs   = 0:0.2:3.2; % nucleation rate
+rs   = 0:0.5:2.5; % nucleation rate
 dim  = 1;  % dimension of system
-
 
 for i = 1:length(rs) 
     
     r = rs(i);
-    [x, tpoints, sump, r_c, v_theor] = solve_advection(v1,v2,fcat,fres,r,dim);
+    
+    [r_c, v_theor, J] = theoretical(v1,v2,fcat,fres,r);
+    
+    [x, tpoints, sump] = solve_advection(v1,v2,fcat,fres,r,dim,r_c,v_theor);
     v_sim = extractV(x,tpoints,sump);
 
     figure(1); hold on;
--- theoretical.m
+++ theoretical.m
@@ -1,14 +1,39 @@
-function [r_critical, v_theoretical] = theoretical( v1,v2,fcat,fres,r )
+function [r_critical, v_theoretical, J] = theoretical( v1,v2,fcat,fres,r )
 %THEORETICAL Summary of this function goes here
 %   Detailed explanation goes here
 
+%% check bounded vs unbounded
+J = (v1*fres-v2*fcat)/(fres+fcat);
+
 %% calculations based on analytical solutions
 
-r_critical = (sqrt(fcat)-sqrt(v1/v2*fres))^2;
-fp_curr = fcat; fm = fres; r_curr = r; vp = v1; vm = v2;
-k_curr = ((fp_curr+fm-r_curr)*sqrt(r_curr*fp_curr)+r_curr*(-fp_curr+fm+r_curr))/(vp+vm)/(fp_curr-r_curr);
-s_curr = (2*r_curr*fm)/(fp_curr+fm-r_curr)-k_curr*(vm*fp_curr-vp*fm-r_curr*vm)/(fp_curr+fm-r_curr);
-v_theoretical = s_curr/k_curr;
+if J < 0
+    % bounded dynamics
+    r_critical = (sqrt(fcat)-sqrt(v1/v2*fres))^2;
+    
+    if r > r_critical
+        
+        if r <= fcat
+            fp_curr = fcat; fm = fres; r_curr = r; vp = v1; vm = v2;
+            k_curr = ((fp_curr+fm-r_curr)*sqrt(r_curr*fp_curr)+r_curr*(-fp_curr+fm+r_curr))/(vp+vm)/(fp_curr-r_curr);
+            s_curr = (2*r_curr*fm)/(fp_curr+fm-r_curr)-k_curr*(vm*fp_curr-vp*fm-r_curr*vm)/(fp_curr+fm-r_curr);
+            v_theoretical = s_curr/k_curr;
+        else
+            v_theoretical = v1; % when r > fcat
+        end
+        
+    else
+        v_theoretical = 0; % when 0 < r < r_c
+    end
+    
+else
+    % unbounded dynamics
+    r_critical = 0;
+    v_theoretical = v1; 
+end
+
+
+
 
 end
 
